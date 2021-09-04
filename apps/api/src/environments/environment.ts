20486a3347c994e0a3b063e50d5a578d01373a58
dyk <359743984@qq.com>
2021-09-04 15:59:26 +0800

    chore: add orm config

--- apps/api/src/environments/environment.ts
+++ apps/api/src/environments/environment.ts
@@ -1,3 +1,15 @@
 export const environment = {
   production: false,
+  orm: {
+    type: 'mysql',
+    host: 'test',
+    port: 3306,
+    username: 'root',
+    password: '123456',
+    database: 'blog',
+    entities: [],
+    synchronize: false,
+  },
 };
+
+export type Env = typeof environment;
