afd0780624fff8add5a17693b6c27e9a21896995
dyk <359743984@qq.com>
2021-09-05 20:17:31 +0800

    chore: import modules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HomeController } from './modules/home/home.controller';
+import { BlogsController } from './modules/blogs/blogs.controller';
+import { BlogsService } from './modules/blogs/blogs.service';
+import { getEnv } from '../environments';
+import { entities } from './app.entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({ ...(getEnv('orm') as any), entities }),
+    TypeOrmModule.forFeature(entities),
+    AuthModule,
+  ],
+  controllers: [AppController, HomeController, BlogsController],
+  providers: [AppService, BlogsService],
 })
 export class AppModule {}
